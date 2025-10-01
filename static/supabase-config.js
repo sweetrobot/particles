43e3355b18baceb72a2c1f69766bfbbd7afc83fb
sweetrobot <stefan.trifonov@gmail.com>
2025-09-30 20:40:49 -0400

    Added supabase-config.js

---
+++ static/supabase-config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = 'https://ryidnlxzevyhfvabvfwh.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5aWRubHh6ZXZ5aGZ2YWJ2ZndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNzI3OTcsImV4cCI6MjA3NDg0ODc5N30.kq_d-2JNklQs4u1uqQo_lD7jRQW1elSIvYh387a3o3A';
